b9504e12aa5eee69f3fd49bff41740bc22757733
Gertobin11 <gertobin11@gmail.com>
2021-05-16 15:20:15 +0000

    connected flask to mongodb

--- app.py
+++ app.py
@@ -15,10 +15,11 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find(tasks)
+    tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
 
